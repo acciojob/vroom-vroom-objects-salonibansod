5daf6e4490d15ee586edb059ad6e0490e897fcab
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-02-03 17:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
-	
+	this.make=make;
+	this.model=model;
 }
-car.prototype.getMakeModel=function(){
+Car.prototype.getMakeModel=function(){
 	return'${this.make} ${this.model}';
 	
 };
 
 function SportsCar(make, model, topSpeed) {
-	car.call(this,make,model);
+	Car.call(this,make,model);
 	this.topSpeed=topSpeed;
 }
 SportsCar.prototype=Object.create(Car.prototype);
